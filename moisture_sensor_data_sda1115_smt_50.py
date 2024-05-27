5707f5a3059d5d8c13c972d4bd6344b6b4f64974
Knesch <bernhardschenk@gmx.de>
2024-05-27 14:34:22 +0200

    refactor: extract some Constants

--- moisture_sensor_data_sda1115_smt_50.py
+++ moisture_sensor_data_sda1115_smt_50.py
@@ -15,7 +15,6 @@ from smbus2 import SMBus
 
 # ADS1115 + hardware constants
 I2C_BUS = 1
-DEVICE_ADDRESS = 0x48
 POINTER_CONVERSION = 0x0
 POINTER_CONFIGURATION = 0x1
 
@@ -24,8 +23,15 @@ RESET_COMMAND = 0b00000110
 # END ADS1115 + hardware constants
 
 SENSOR_DATA_PATH = "./static/data/moisture_sensor_data"
+
+# Settings - ToDo make configurable
 SENSOR_NAME = "sda1115_smt_50"
-MAX_VOLTAGE = 3
+SENSOR_DEVICE_ADDRESS = 0x48
+SENSOR_READ_INTERVALL_MINUTES = 5
+SENSOR_MAX_VOLTAGE = 3
+SENSOR_DRIEST = 0
+SENSOR_WETTEST = 50
+SENSOR_CHANNEL = 0
 
 msd_signal = signal("moisture_sensor_data")
 
@@ -33,17 +39,14 @@ def moisture_sensor_data_init():
     if not os.path.isdir(SENSOR_DATA_PATH):
         os.makedirs(SENSOR_DATA_PATH, exist_ok=True)
 
-        sensor_file = f"{SENSOR_DATA_PATH}/{SENSOR_NAME}"
-        if not os.path.isfile(sensor_file):
-            create_sensor_data_file(sensor_file)
+    sensor_file = f"{SENSOR_DATA_PATH}/{SENSOR_NAME}"
+    if not os.path.isfile(sensor_file):
+        create_sensor_data_file(sensor_file)
 
 def create_sensor_data_file(new_file):
     """Use x and y as headings for the graph plugin"""
     with open(new_file, "w") as f:
         f.write("x,y\n")
-
-
-
 
 #ADS1115 functions
 def prepareLEconf(BEconf):
@@ -70,10 +73,10 @@ def read_channel():
     # start single conversion - AIN0/GND - 4.096V - single shot - 8SPS - X
     # - X - X - disable comparator
     conf = prepareLEconf('1-100-001-1-000-0-0-0-11')
-    bus.write_word_data(DEVICE_ADDRESS, POINTER_CONFIGURATION, conf)
+    bus.write_word_data(SENSOR_DEVICE_ADDRESS, POINTER_CONFIGURATION, conf)
     # long enough to be safe that data acquisition (conversion) has completed
     time.sleep(1)
-    value_raw = bus.read_word_data(DEVICE_ADDRESS, POINTER_CONVERSION)
+    value_raw = bus.read_word_data(SENSOR_DEVICE_ADDRESS, POINTER_CONVERSION)
     bus.close()
     value = LEtoBE(value_raw)
     voltage = value / pow(2,15) * 4.096
@@ -84,7 +87,7 @@ def read_loop():
     while True:
         ts_secs = int(gv.now)
         currentVoltage = read_channel()
-        percent = currentVoltage / MAX_VOLTAGE * 100 / 2 
+        percent = currentVoltage / SENSOR_MAX_VOLTAGE * 100 / 2
         percent = round(percent)
         msd_signal.send(
             "reading", data={"sensor": SENSOR_NAME, "timestamp": ts_secs, "value": percent}
@@ -92,7 +95,7 @@ def read_loop():
         sensor_file = f"{SENSOR_DATA_PATH}/{SENSOR_NAME}"
         with open(sensor_file, "a") as f:
             f.write(f"{ts_secs * 1000},{percent}\n")
-        time.sleep(60 * 5)
+        time.sleep(60 * SENSOR_READ_INTERVALL_MINUTES)
 
 moisture_sensor_data_init()
 
